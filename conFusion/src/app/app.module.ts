cf234be9a858265b767d3611bd717ad552a61050
Fabio Saraseli <fabiooosara@gmail.com>
2022-08-16 14:00:44 -0700

    Directives

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -41,6 +41,7 @@ import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,7 +52,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
